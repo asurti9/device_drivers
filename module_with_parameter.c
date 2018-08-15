753135f38c66a07fe78f6230d4aa0a11fb7dcd37
Aliasgar Surti <aliasgar.surti500@gmail.com>
2018-08-15 13:27:36 +0530

    Added module accepting parameters

---
+++ module_with_parameter.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int howmany = 1;
+static char* whom = "world";
+module_param(howmany,int,S_IRUGO);
+module_param(whom,charp,S_IRUGO);
+
+static int hello_init(void)
+{
+
+	int i;
+	for (i=0;i<howmany;i++)
+		printk(KERN_ALERT "Hello, %s\n",whom);
+	return 0;
+}
+
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
